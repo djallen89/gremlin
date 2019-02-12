bf4b4982b653e84629c071e02d7e4fe9d4b42fda
Dominick Allen <dominick.allen1989@gmail.com>
2019-02-11 19:55:43 -0500

    recieving error \'expected struct `criterion::Criteriontest::Bencher`\'

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -5,8 +5,9 @@ use gremlin::{random_array, matrix_madd};
 use ndarray::Array;
 use ndarray::linalg::general_mat_mul;
 
-use criterion::{Criterion, ParameterizedBenchmark};
+use criterion::{Benchmark, Criterion};
 
+#[bench]
 fn benchdgemm(crit: &mut Criterion) {
     let n = 32;
     let m = 32;
@@ -18,17 +19,18 @@ fn benchdgemm(crit: &mut Criterion) {
     let a_arr = Array::from_vec(a.clone()).into_shape((n, m)).unwrap();
     let b_arr = Array::from_vec(b.clone()).into_shape((n, m)).unwrap();
     let mut c_arr = Array::from_vec(c.clone()).into_shape((n, m)).unwrap();
+
+    let bench_def = Benchmark::new(
+        "my dgemm 32x32",
+        move |bch| bch.iter(|| {
+            matrix_madd(n, m, &a, &b, &mut c)
+        }))
+        .with_function("ndarray dgemm 32x32", move |bch| bch.iter(|| {
+            general_mat_mul(1.0, &a_arr, &b_arr, 1.0, &mut c_arr)
+        }))
+        .sample_size(50);
     
-    crit.bench("dgemm",
-            ParameterizedBenchmark::new("my dgemm 32x32",
-                                        move |bch, i| bch.iter(|| {
-                                            matrix_madd(*i, *i, &a, &b, &mut c)}),
-                                        vec![32, 32, 32])
-            .with_function("ndarray dgemm 32x32",
-                           move |bch, _i| bch.iter(|| {
-                               general_mat_mul(1.0, &a_arr, &b_arr, 1.0, &mut c_arr)
-                           })
-    ));
+    crit.bench("dgemm", bench_def);
 }
 
 criterion_group!(benches, benchdgemm);
