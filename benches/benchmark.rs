0baab2f1109aad4bad1c15215e1a9d0d329d42a5
Dominick Allen <dominick.allen1989@gmail.com>
2019-02-23 15:18:20 -0500

    added various benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -13,25 +13,24 @@ fn bench_n_sq(crit: &mut Criterion, i: usize) {
     let m = i;
 
     let my_name = format!("my dgemm {}sq", n);
-    //let other_name = format!("ndarray {}sq", n);
-    //let naive_name = format!("naive {}sq", n);
+    let other_name = format!("ndarray {}sq", n);
     
     let a: Vec<f64> = random_array(n, m, -100.0, 100.0);
     let b = random_array(n, m, -100.0, 100.0);
     let mut c = random_array(n, m, -100.0, 100.0);
 
-    //let aarr = Array::from_vec(a.clone()).into_shape((n, m)).unwrap();
-    //let barr = Array::from_vec(b.clone()).into_shape((n, m)).unwrap();
-    //let mut carr = Array::from_vec(c.clone()).into_shape((n, m)).unwrap();
+    let aarr = Array::from_vec(a.clone()).into_shape((n, m)).unwrap();
+    let barr = Array::from_vec(b.clone()).into_shape((n, m)).unwrap();
+    let mut carr = Array::from_vec(c.clone()).into_shape((n, m)).unwrap();
 
     let bench_def;
     bench_def = Benchmark::new(
         my_name, move |bch| bch.iter(|| {
             matrix_madd(n, m, n, &a, &b, &mut c)
         }))
-        //.with_function(other_name, move |bch| bch.iter(|| {
-            //general_mat_mul(1.0, &aarr, &barr, 1.0, &mut carr)
-        //}))
+        .with_function(other_name, move |bch| bch.iter(|| {
+            general_mat_mul(1.0, &aarr, &barr, 1.0, &mut carr)
+        }))
         .sample_size(10);
     
     crit.bench("dgemm", bench_def);
@@ -266,6 +265,201 @@ fn bench_1xmx2048(crit: &mut Criterion) {
     bench_nmp(n, m, p, crit);
 }
 
+fn bench_300_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 300);
+}
+
+fn bench_320_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 320);
+}
+
+fn bench_340_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 340);
+}
+
+fn bench_352_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 352);
+}
+
+fn bench_360_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 360);
+}
+
+fn bench_384_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 384);
+}
+
+fn bench_416_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 416);
+}
+
+fn bench_448_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 448);
+}
+
+fn bench_576_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 576);
+}
+    
+fn bench_608_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 544);
+}
+
+fn bench_640_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 640);
+}
+
+fn bench_672_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 672);
+}
+
+fn bench_704_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 704);
+}
+
+fn bench_840_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 840);
+}
+
+fn bench_880_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 880);
+}
+
+fn bench_896_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 896);
+}
+
+fn bench_928_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 928);
+}
+
+fn bench_960_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 960);
+}
+
+fn bench_1088_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1088);
+}
+
+fn bench_1152_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1152);
+}
+
+fn bench_1184_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1184);
+}
+
+fn bench_1216_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1216);
+}
+
+fn bench_1248_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1248);
+}
+
+fn bench_1280_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1280);
+}
+
+//(mapcar (lambda (x) (* x 32)) (list 41 42 43 44 45 46 47 48))
+//(1312 1344 1376 1408 1440 1472 1504 1536)
+
+fn bench_1312_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1312);
+}
+
+fn bench_1344_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1344);
+}
+
+fn bench_1376_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1376);
+}
+criterion_group!(others, bench_300_sq, bench_320_sq, bench_340_sq, bench_352_sq,
+                 bench_360_sq, bench_384_sq, bench_416_sq, bench_448_sq,
+                 bench_576_sq, bench_608_sq, bench_640_sq, bench_672_sq,
+                 bench_704_sq, bench_840_sq, bench_880_sq, bench_896_sq,
+                 bench_928_sq, bench_960_sq, bench_1088_sq, bench_1152_sq,
+                 bench_1184_sq, bench_1216_sq, bench_1248_sq, bench_1280_sq,
+                 bench_1312_sq, bench_1344_sq, bench_1376_sq);
+
+fn bench_1000_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1000);
+}
+
+fn bench_1008_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1008);
+}
+
+fn bench_1016_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1016);
+}
+
+fn bench_1020_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1020);
+}
+
+fn bench_1021_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1021);
+}
+
+fn bench_1022_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1022);
+}
+
+fn bench_1023_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1023);
+}
+
+fn bench_1025_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1025);
+}
+
+fn bench_1026_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1026);
+}
+
+fn bench_1027_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1027);
+}
+
+fn bench_1028_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1028);
+}
+
+fn bench_1032_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1032);
+}
+
+fn bench_1036_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 1036);
+}
+
+fn bench_2000_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 2000);
+}
+
+fn bench_2200_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 2200);
+}
+
+fn bench_2400_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 2400);
+}
+
+fn bench_2600_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 2600);
+}
+
+fn bench_2800_sq(crit: &mut Criterion) {
+    bench_n_sq(crit, 2800);
+}
+
+criterion_group!(thousand, bench_1000_sq, bench_1008_sq, bench_1016_sq,
+                 bench_1020_sq, bench_1021_sq, bench_1022_sq, bench_1023_sq,
+                 bench_1025_sq, bench_1026_sq, bench_1027_sq, bench_1028_sq,
+                 bench_1032_sq, bench_1036_sq, bench_1376_sq);
+
 criterion_group!(small_non4_matrices, bench_27_sq, bench_29_sq, bench_30_sq,
                  bench_31_sq, bench_33_sq, bench_34_sq, bench_35_sq);
 criterion_group!(small_4x_matrices, bench4x4, bench_12_sq, bench_16_sq,
@@ -286,12 +480,13 @@ criterion_group!(very_big_matrices, bench_736_sq, bench_768_sq,
 criterion_group!(vectors, bench_2048x1, bench_1xmx2048);
 criterion_group!(various_4x, bench_32_sq, bench_64_sq, bench_128_sq,
                  bench_256_sq, bench_512_sq, bench_768_sq, bench_1024_sq);
-
+/*
 criterion_main!(small_non4_matrices, small_4x_matrices, mid_non4_matrices,
                 mid_4x_matrices, big_4x_matrices, very_big_matrices,
-                vectors, various_4x);
-
-//criterion_main!(very_big_matrices, big_4x_matrices, mid_4x_matrices, 
-//                vectors, various_4x);
-//criterion_main!(various_4x);
-//criterion_main!(small_4x_matrices);
+                vectors);
+ */
+//criterion_main!(others);
+criterion_group!(t26, bench_1026_sq);
+criterion_group!(twok, bench_2000_sq, bench_2200_sq, bench_2400_sq,
+                 bench_2600_sq, bench_2800_sq);
+criterion_main!(twok);
