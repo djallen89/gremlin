842419f3590b7a22303952df3f61a5d3321d4cb4
Dominick Allen <dominick.allen1989@gmail.com>
2019-02-24 20:17:38 -0500

    removed commented-out code

--- src/lib/danger_math.rs
+++ src/lib/danger_math.rs
@@ -1,29 +1,5 @@
 pub struct Ptr(usize);
 
-/*
-impl Ptr {
-    pub fn from_addr(borrow_addr: &f64) -> Ptr {
-        Ptr {
-            addr: (borrow_addr as *const f64) as usize
-        }
-    }
-
-    pub fn from_ptr(ptr: *const f64) -> Ptr {
-        Ptr {
-            addr: ptr as usize
-        }
-    }
-
-    pub fn as_ptr(&self) -> *const f64 {
-        self.addr as *const f64
-    }
-
-    pub fn as_mut_ptr(&self) -> *mut f64 {
-        self.addr as *mut f64
-    }
-}
- */
-
 impl Ptr {
     pub fn from_addr(borrow_addr: &f64) -> Ptr {
         Ptr((borrow_addr as *const f64) as usize)
